64f4a75339a3fed5422259f50eab18927a0eeaf1
Prahlad Gaur <65204011+prahladgaur@users.noreply.github.com>
2023-12-07 23:32:50 +0530

    Update index.js
    k

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 //console.log("hello prahlad");
-require('dotenv').config()
+//require('dotenv').config()
 const express = require("express");
 const app = express();
 
-
-
-//const port = 4000;
+const port = 3000;
 
 app.get('/',(req,res)=>{
     res.send('hello prahlad 2')
@@ -15,6 +13,6 @@ app.get('/paw',(req,res)=>{
   res.send('<h1>Hello Prahlad</h1>')
 })
 
-app.listen(process.env.PORT, ()=>{
-    console.log(`listening on ${process.env.PORT}`)
+app.listen(port, ()=>{
+    console.log(`listening on ${port}`)
 })
